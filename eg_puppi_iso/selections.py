eac57b04f1e559a3ec69a923032294d5ed3c1d42
A.R.Sahasransu <a.r.sahasransu@gmail.com>
2024-11-20 10:21:41 +0000

    Add Gen selection for DYToLL

--- eg_puppi_iso/selections.py
+++ eg_puppi_iso/selections.py
@@ -1,5 +1,5 @@
 from python.selections import Selection
 
 # Select for events with prompt gen electrons
-gen_prompt_el_sel = [Selection('GenPromptE', 'isPrompt', lambda array: array.prompt == 2),
-                     Selection('GenPromptTauToE', 'PromptTauToE', lambda array: array.prompt == 1)]
+gen_prompt_el_sel = [Selection('GenPromptE', 'isPrompt', 
+                               lambda array: ( (array.prompt==2) & (abs(array.eta)<1.48) ))]
