6dfc60143080c175c19a1e725d693349ce60d34a
A.R.Sahasransu <a.r.sahasransu@gmail.com>
2024-11-20 16:47:01 +0000

    Big fix while adding Puppi collections

--- cfg/egiso_puppi.py
+++ cfg/egiso_puppi.py
@@ -1,8 +1,10 @@
-from eg_puppi_iso.collections import gen_ele
-from eg_puppi_iso.plotters import GenPlotter
+from eg_puppi_iso.collections import gen_ele, puppi_ele
+from eg_puppi_iso.plotters import GenPlotter, PuppiPlotter
 from eg_puppi_iso.selections import gen_prompt_el_sel
 
 from python.selections import Selection
 
 all_genel_plotter = [GenPlotter(gen_ele, [Selection('all')])]
 prompt_genel_plotter = [GenPlotter(gen_ele, gen_prompt_el_sel)]
+
+all_puppi_plotter = [PuppiPlotter(puppi_ele, [Selection('all')])]
--- eg_puppi_iso/plotters.py
+++ eg_puppi_iso/plotters.py
@@ -1,6 +1,6 @@
 from python.plotters import GenericDataFramePlotter
 
-from .histos import CaloGenParticleHistos
+from .histos import CaloGenParticleHistos, ParticleHistos
 
 class GenPlotter(GenericDataFramePlotter):
     def __init__(self, gen_set, gen_selections, pt_bins=None):
@@ -8,4 +8,12 @@ class GenPlotter(GenericDataFramePlotter):
             CaloGenParticleHistos,
             gen_set,
             gen_selections,
+            pt_bins)
+
+class PuppiPlotter(GenericDataFramePlotter):
+    def __init__(self, puppi_set, puppi_selections, pt_bins=None):
+        super(PuppiPlotter, self).__init__(
+            ParticleHistos,
+            puppi_set,
+            puppi_selections,
             pt_bins)
