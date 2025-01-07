2b1b1131ff4114b87036892d7fdaf4ad95258a02
a.r.sahasransu@gmail.com <a.r.sahasransu@gmail.com>
2025-01-07 14:20:49 +0000

    EB only for PuppiEl

--- cfg/egiso_puppi.py
+++ cfg/egiso_puppi.py
@@ -1,10 +1,10 @@
 from eg_puppi_iso.collections import gen_ele, puppi_ele
 from eg_puppi_iso.plotters import GenPlotter, PuppiPlotter
-from eg_puppi_iso.selections import gen_prompt_el_sel
+from eg_puppi_iso.selections import gen_prompt_el_sel, el_eb_sel
 
 from python.selections import Selection
 
 all_genel_plotter = [GenPlotter(gen_ele, [Selection('all')])]
 prompt_genel_plotter = [GenPlotter(gen_ele, gen_prompt_el_sel)]
 
-all_puppi_plotter = [PuppiPlotter(puppi_ele, [Selection('all')])]
+all_puppi_plotter = [PuppiPlotter(puppi_ele, el_eb_sel)]
--- eg_puppi_iso/selections.py
+++ eg_puppi_iso/selections.py
@@ -3,3 +3,5 @@ from python.selections import Selection
 # Select for events with prompt gen electrons
 gen_prompt_el_sel = [Selection('GenPromptE', 'isPrompt', 
                                lambda array: ( (array.prompt==2) & (abs(array.eta)<1.48) ))]
+
+el_eb_sel = [Selection('EB', 'EB', lambda array: (abs(array.eta) < 1.48))]
